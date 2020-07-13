46049867e02065b22c9ca032bc4318660f2c0b0a
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-13 18:13:18 -0300

    exercicio 1 de criação das 5 operações

---
+++ 4.1 Introdução a Javascript/exercises/4_1/script.js
@@ -0,0 +1,9 @@
+let x = 10
+let y = 15
+
+console.log('Soma: ' + (x + y));
+console.log('Subtração: ' + (x - y));
+console.log('Multiplicação: ' + (x * y));
+console.log('Divisão: ' + (x / y));
+console.log('Módulo: ' + (x % y));
+
