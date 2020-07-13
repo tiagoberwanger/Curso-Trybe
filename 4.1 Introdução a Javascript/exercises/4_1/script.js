d0d66d73e528f6ffb573f1d6bebbcd127c0018ad
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-13 19:00:12 -0300

    exercicio 6 das peças de xadrez

--- 4.1 Introdução a Javascript/exercises/4_1/script.js
+++ 4.1 Introdução a Javascript/exercises/4_1/script.js
@@ -41,10 +41,38 @@ let x = 30
 let y = 40
 let z = 50
 
-if (x + y + z == 180){
+if (x + y + z === 180){
     console.log (true);
 } else {
     console.log (false);
 }
+
+let pecaDeXadrez = 'bispo';
+
+switch (pecaDeXadrez.toLowerCase()) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal e horizontal.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+}
+
+
 
 
