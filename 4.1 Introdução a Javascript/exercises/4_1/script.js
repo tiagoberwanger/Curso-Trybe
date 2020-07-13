77a0723fc78ce353a85b6b102876372da21f4d56
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-13 18:25:07 -0300

    exercicio 2 de retornar o maior número

--- 4.1 Introdução a Javascript/exercises/4_1/script.js
+++ 4.1 Introdução a Javascript/exercises/4_1/script.js
@@ -1,9 +1,18 @@
-let x = 10
-let y = 15
+let x = 10;
+let y = 15;
 
 console.log('Soma: ' + (x + y));
 console.log('Subtração: ' + (x - y));
 console.log('Multiplicação: ' + (x * y));
 console.log('Divisão: ' + (x / y));
 console.log('Módulo: ' + (x % y));
+
+let x = 20;
+let y = 25;
+
+if (x > y) {
+    console.log ("x é maior que y")
+} else {
+    console.log("y é maior que x")
+}
 
