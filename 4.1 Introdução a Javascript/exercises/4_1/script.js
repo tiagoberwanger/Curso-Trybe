044e338f484775c0c8fc2f3fcdc9dd9cad7ee0c0
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-13 19:19:11 -0300

    exercicio 7 sobre as notas

--- 4.1 Introdução a Javascript/exercises/4_1/script.js
+++ 4.1 Introdução a Javascript/exercises/4_1/script.js
@@ -97,6 +97,24 @@ switch (pecaDeXadrez.toLowerCase()) {
     break;
 }
 
+// Exercício 7
 
+let nota = 80;
+
+if (nota >= 90 && nota <= 100) {
+    console.log("A")
+} else if (nota >= 80 && nota < 90){
+    console.log("B")
+} else if (nota >= 70 && nota < 80){
+    console.log("C")
+} else if (nota >= 60 && nota < 70){
+    console.log("D")
+} else if (nota >= 50 && nota < 60){
+    console.log("E")
+} else if (nota >= 0 && nota < 50){
+    console.log("F")
+} else {
+    console.log("nota inválida")
+}
 
 
