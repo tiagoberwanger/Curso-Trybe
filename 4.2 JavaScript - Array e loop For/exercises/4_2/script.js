e405b6451dfb7e30d4051cca0194652c655acbf7
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-14 19:31:43 -0300

    Exercício 5 descubra o maior valor

--- 4.2 JavaScript - Array e loop For/exercises/4_2/script.js
+++ 4.2 JavaScript - Array e loop For/exercises/4_2/script.js
@@ -26,6 +26,19 @@ console.log(mediaAritmética)
 
 //Exercício 4
 
+if (mediaAritmética >20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor ou igual a 20")
+}
+
+//Exercício5
+
+let maiorValor = Math.max(5, 9, 3, 19, 70, 8, 100, 2, 35, 27);
+
+console.log(maiorValor);
+
+
 
 
 
