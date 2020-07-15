4837306b6b8f3f8b7615bd55f9ea53f6c9e862b3
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-15 12:12:16 -0300

    Ajustes finais nos exercícios

--- 4.2 JavaScript - Array e loop For/exercises/4_2/script.js
+++ 4.2 JavaScript - Array e loop For/exercises/4_2/script.js
@@ -34,10 +34,24 @@ if (mediaAritmética >20) {
 
 //Exercício 5
 
+//v1
+
 let maiorValor = Math.max(5, 9, 3, 19, 70, 8, 100, 2, 35, 27);
 
 console.log(maiorValor);
 
+//v2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0
+
+for (i = 0; i < numbers.length; i++){
+    if (numbers[i] > higherNumber) {
+        higherNumber = numbers[i];
+    }
+}
+
+console.log("O maior número do array é " + higherNumber)
 
 //Exercício 6 
 
@@ -46,14 +60,31 @@ let count = 0
 
 for (i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 !== 0) {
-        console.log(numbers[i])
+        count += 1
     } else {
         console.log ("nenhum valor ímpar encontrado");
     }
 }
 
+console.log("O total de números ímpares é " + count)
+
 //Exercício 7
+
+//v1
 
 let menorValor = Math.min(5, 9, 3, 19, 70, 8, 100, 2, 35, 27);
 
 console.log(menorValor);
+
+//v2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lowerNumber = 1000;
+
+for (i = 0; i < numbers.length; i++){
+    if (numbers[i] < lowerNumber) {
+        lowerNumber = numbers[i];
+    }
+}
+
+console.log("O menor número do array é " + lowerNumber)
