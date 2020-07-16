da3a8c0ad23dc545ef6869521f01cde8373a054f
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-16 19:16:41 -0300

    Exercício 1 - Parte I

--- 4.4 JavaScript - Objetos e funções/exercises/4_4/exercicios.js
+++ 4.4 JavaScript - Objetos e funções/exercises/4_4/exercicios.js
@@ -1,1 +1,14 @@
 //Exercícios 4.4 JavaScript - Objetos e funções
+
+//Parte I - Objetos e For/In
+
+//Exercício 1
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  console.log("Bem vinda, " + info.personagem)
+
