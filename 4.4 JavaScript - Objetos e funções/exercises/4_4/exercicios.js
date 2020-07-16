1e7b99ebd298f9a6bc8a68948659a417818af639
Tiago Berwanger <berwangertiago@gmail.com>
2020-07-16 19:39:01 -0300

    Exercício 5 - Parte I

--- 4.4 JavaScript - Objetos e funções/exercises/4_4/exercicios.js
+++ 4.4 JavaScript - Objetos e funções/exercises/4_4/exercicios.js
@@ -31,3 +31,26 @@ for(let propriedade in info) {
     console.log(info[propriedade])
 }
 
+//Exercício 5
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+  };
+  let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+  };
+  
+  console.log(info.personagem + " e " + info2.personagem)
+  console.log(info.origem + " e " + info2.origem)
+  console.log(info.nota + " e " + info2.nota)
+  if(info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+    console.log('Ambos recorrentes')
+  }
+
+  
