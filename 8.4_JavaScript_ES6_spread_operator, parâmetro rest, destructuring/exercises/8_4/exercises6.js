ab7e282692a0d346a0e2bac1c0a043ed7e8898f4
Tiago Berwanger <berwangertiago@gmail.com>
2020-08-21 01:11:03 -0300

    Exercícios 5 e 6

--- 8.4_JavaScript_ES6_spread_operator, parâmetro rest, destructuring/exercises/8_4/exercises6.js
+++ 8.4_JavaScript_ES6_spread_operator, parâmetro rest, destructuring/exercises/8_4/exercises6.js
@@ -6,7 +6,7 @@ const palio = ["Palio", "Fiat", 2019]
 const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
 const chiron = ["Chiron", "Bugatti", 2016]
 
-// escreva toObject abaixo
+    const toObject = car => ({name: car[0], brand: car[1], year: car[2]})
 
 assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
 assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
