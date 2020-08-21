70673912c3c92bbad98600bb5f4c16dd7e960cb9
Tiago Berwanger <berwangertiago@gmail.com>
2020-08-21 01:23:14 -0300

    Exercícios 7 e 8

--- 8.4_JavaScript_ES6_spread_operator, parâmetro rest, destructuring/exercises/8_4/exercises8.js
+++ 8.4_JavaScript_ES6_spread_operator, parâmetro rest, destructuring/exercises/8_4/exercises8.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert')
 
-// escreva greet abaixo
+const greet = (name, greeting = "Hi") => `${greeting} ${name}`
 
 assert.equal(greet("John"), "Hi John")
 assert.equal(greet("John", "Good morning"), "Good morning John")
