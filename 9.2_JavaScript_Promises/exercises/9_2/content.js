9f87ba2592abb8181bd6e5b584c30fe4aee87176
Tiago Berwanger <berwangertiago@gmail.com>
2020-08-28 01:00:26 -0300

    subindo exercícios módulo 9.2

--- 9.2_JavaScript_Promises/exercises/9_2/content.js
+++ 9.2_JavaScript_Promises/exercises/9_2/content.js
@@ -1,1 +1,35 @@
-//start
+//Uso de Promises(resolve, reject), then e catch.
+const promise = new Promise((resolve, reject) => {
+    const number = Math.floor(Math.random()* 11);
+  
+    if (number > 10 || number <= 5) {
+      return reject(number);
+    }
+    resolve(number);
+  })
+  .then(number => `Que sucesso =) nosso número foi ${number}`)
+  .then(msg => console.log(msg))
+  .catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
+  
+
+//Uso do fetch, await e async para retornar uma Promise(resolve, reject).
+
+const fetch = require('node-fetch');
+
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => (r.value));
+  }
+  throw new Error('endpoint não existe');
+}
+
+async function sendJokeToFriend(name) {
+  const message = await verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna")
