9bc84889d0a9a380b7885b4529846bd9e6de01ad
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-02 19:20:28 -0300

    iniciando exercícios módulo 10.1

---
+++ 10.1_Primeiros_passos_no_Jest/exercises/10_1/exercise1.1.js
@@ -0,0 +1,28 @@
+//Exercise 1 
+
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+//REFATORAR PARA JEST
+
+assert.strictEqual(typeof sum, 'function');
+
+assert.strictEqual(sum(4, 5), 9);
+
+assert.strictEqual(sum(0, 0), 0);
+
+assert.throws (function () {
+    sum(4, "5");
+})
+
+assert.throws (function () {
+  sum(4, "5");
+}, /paramenters must be numbers/)
+
