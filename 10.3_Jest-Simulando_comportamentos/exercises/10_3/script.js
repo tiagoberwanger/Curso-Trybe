4bfd12c84dc3438361b6de99896eac3add98d04e
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-07 13:43:51 -0300

    subindo exercícios 1 ao 4 - módulo 10.3

--- 10.3_Jest-Simulando_comportamentos/exercises/10_3/script.js
+++ 10.3_Jest-Simulando_comportamentos/exercises/10_3/script.js
@@ -1,1 +1,16 @@
-// Iniciando exercícios módulo 10.3
+// Crie uma função que gere um número aleatório.
+
+const randomNumber = () => {
+    return parseInt(Math.random()*100)
+}
+
+module.exports = randomNumber;
+// Crie três funções com os seguintes comportamentos
+
+const uppercased = (str) => str.toUpperCase();
+
+const firstLetter = (str) => str.charAt(0)
+
+const concatenate = (str1, str2) => str1 + str2;
+
+module.exports = {uppercased, firstLetter, concatenate};
