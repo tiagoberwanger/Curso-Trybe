22224b5b877dc4a57bb8161b6f0f183c4f81bc10
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-21 19:29:36 -0300

    iniciando o exercício módulo 13.1

---
+++ 13.1_Ciclo_de_vida_de_componentes/exercises/13_1/exercise-dog-image/src/Doguineo.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Doguineo extends React.Component  {
+
+render() {
+  return (
+    <h1> Hello, World.</h1>
+    )
+  }
+}
+
+
+export default Doguineo;
