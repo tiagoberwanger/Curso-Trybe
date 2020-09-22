a8021669c6ee12b856a16412443c4741c014d90a
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-22 01:14:21 -0300

    subindo o exercício módulo 13.1

--- 13.1_Ciclo_de_vida_de_componentes/exercises/13_1/exercise-dog-image/src/Doguineo.js
+++ 13.1_Ciclo_de_vida_de_componentes/exercises/13_1/exercise-dog-image/src/Doguineo.js
@@ -1,14 +1,36 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 
 class Doguineo extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+    this.fetchDog = this.fetchDog.bind(this);
+  }
 
-render() {
-  return (
-    <h1> Hello, World.</h1>
-    )
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  fetchDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(result => this.setState({ data: result }));
+  }
+
+  render() {
+    if (this.state.data === "") return "loading...";
+    return (
+      <div>
+        <h1>Doguineos</h1>
+        <button onClick={this.fetchDog}>Novo doguineo :)</button>
+        <div>
+          <img src={this.state.data.message} />
+        </div>
+      </div>
+    );
   }
 }
-
 
 export default Doguineo;
