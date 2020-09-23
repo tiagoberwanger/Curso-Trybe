c27e089c811cc46cefb39f95508dd1a3f12133fb
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-22 23:21:23 -0300

    exercícios de fixação do 1 ao 6

--- 13.2_React_Router/exercises/13_2/exercise-router-fixation/src/App.js
+++ 13.2_React_Router/exercises/13_2/exercise-router-fixation/src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+import Users from './Users';
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <div className="main-div">
+        <BrowserRouter>
+        <header className="header">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/users">Users</Link>
+        </header>
+        <section className="main-content">
+          <Route exact path="/"> 
+            <Home /> 
+          </Route>
+          <Route exact path="/about"> 
+            <About /> 
+          </Route>
+          <Route exact path="/users"> 
+            <Users /> 
+          </Route>
+        </section>
+        </BrowserRouter>
+      </div>
     );
   }
 }
