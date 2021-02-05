817f475d0c22af3caa31419b07947f36e5f23176
Tiago Berwanger <berwangertiago@gmail.com>
2021-02-05 13:56:21 -0300

    Exercicios 1, 2, 3

--- 26.4_NodeJS_HTTP_Express/index.js
+++ 26.4_NodeJS_HTTP_Express/index.js
@@ -1,9 +1,21 @@
-// HELLO WORLD com Express
+//EXERCICIO 2
 const express = require('express');
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/ping', function (req, res) {
+  res.send('Pong!')
 })
  
+app.listen(3000);
+
+//EXERCICIO 3
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  // const name = req.body.name;
+  res.status(200).json({ "message": `Hello, ${name}!` })
+})
+
 app.listen(3000);
