ffcbfdbcbeeac4379e9db3d06f9b29e6d113f709
Tiago Berwanger <berwangertiago@gmail.com>
2021-02-26 17:08:27 -0300

    Conteudo 28.2

---
+++ 28.2NodeJS-Upload_com_Multer/multer-io/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+const storage = multer.diskStorage({ 
+  destination: (req, file, callback) => {
+    callback(null, "uploads/")
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage })
+
+app.post('/upload', upload.single('arquivo'), (req, res) => {
+  console.log(req.body, req.file)
+  res.send('Ok!')
+});
+
+app.get('/ping', (req, res) => res.send('pong!'));
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Running at port: ${PORT}`));
