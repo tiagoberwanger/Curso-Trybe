32b7d1ab389f6bd3276f1c631d3fa6dd69d5c30e
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-29 18:29:11 -0300

    example done

--- 31.3_ORM_Associations/exemplo_conteudo/index.js
+++ 31.3_ORM_Associations/exemplo_conteudo/index.js
@@ -1,6 +1,7 @@
 // index.js
 const express = require('express');
 const { Addresses, Employees } = require('./models');
+const sequelize = require('sequelize');
 
 const app = express();
 
@@ -23,7 +24,39 @@ app.get('/employees/:id', (req, res) => Employees
     }
     return res.status(200).json(employee);
   })
-  .catch(() => res.status(500).json({ message: 'Algo deu errado' })));
+  .catch(() => res.status(500).json({ message: 'Algo deu errado' }))
+);
+
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    // Depois executamos as operações
+    const employee = Employee.create(
+      { firstName, lastName, age },
+      { transaction: t }
+    );
+
+    const address = Address.create(
+      { city, street, number, employee_id: employee.id },
+      { transaction: t }
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    res.status(200).json({message: 'Cadastrado com sucesso'});
+  } catch (_e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    await t.rollback();
+    res.status(500).json({ message: 'Algo deu errado' })
+  }  
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Port: ${PORT}`));
