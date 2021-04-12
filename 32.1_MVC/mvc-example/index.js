e691121ec01d2f42341dfbaa899143bb93dd437b
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-12 16:25:53 -0300

    ajusting git

--- 32.1_MVC/mvc-example/index.js
+++ 32.1_MVC/mvc-example/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const authorController = require('./controllers/authorController');
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
 
