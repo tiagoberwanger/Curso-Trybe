5d763e53f87e742849da7a30a0352ded2aa316c6
Tiago Berwanger <berwangertiago@gmail.com>
2021-05-11 14:23:40 -0300

    exercise 2

--- 35.2_Input_Output/exercises/exercise2.py
+++ 35.2_Input_Output/exercises/exercise2.py
@@ -1,1 +1,15 @@
 # não entendi nada
+import random
+
+right_word = "paralelepipedo"
+
+scrambled_word = "".join(random.sample(right_word, len(right_word)))
+print(scrambled_word)
+
+guessed_word = ""
+while guessed_word != right_word:
+    guessed_word = input("Que palavra é essa? ")
+    if (guessed_word != right_word):
+        print("Tente novamente...")
+
+print("Parabéns, você acertou!")
