092ff2c3c1ca79aa03065b7e8a9b42775ea8e00d
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-01 13:44:37 -0300

    exercise 3 - refactored

--- 37.2_Recursividade_Python/exercises/exercise3.py
+++ 37.2_Recursividade_Python/exercises/exercise3.py
@@ -8,3 +8,15 @@ def bigger_value(list):
 
 
 print(bigger_value([4, 30, 1, 200, 5]))
+
+
+# def maior_inteiro(lista):
+
+# if len(lista) == 1:
+# return lista[0]
+# else:
+# maior = maior_inteiro(lista[1:])
+# if maior > lista[0]:
+# return maior
+# else:
+# return lista[0]
