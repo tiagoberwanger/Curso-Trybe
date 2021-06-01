d529a8b7240f0d9b5ef54d10cb272c1d3976b862
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-01 13:23:46 -0300

    exercise 2 - refactored

--- 37.2_Recursividade_Python/exercises/exercise2.py
+++ 37.2_Recursividade_Python/exercises/exercise2.py
@@ -1,11 +1,11 @@
-# porque não funciona?
 def even_counter_recursive(n):
     counter = 0
-    while (n > 1):
+    if (n == 1):
+        return 0
+    else:
         if n % 2 == 0:
             counter += 1
-        even_counter_recursive(n-1)
-    return counter
+        return counter + even_counter_recursive(n-1)
 
 
 print(even_counter_recursive(29))
